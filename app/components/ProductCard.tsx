bed406b791162ead686643fd3ab6a951e1896159
Salino3 <flaviobcn012@gmail.com>
2026-05-02 12:56:25 +0200

    save changes

--- app/components/ProductCard.tsx
+++ app/components/ProductCard.tsx
@@ -1,10 +1,11 @@
 "use client";
 import React from "react";
 import AddToCart from "./AddToCart";
+import styles from "./ProductCard.module.css";
 
 const ProductCard: React.FC = () => {
   return (
-    <div className="rootProductCard">
+    <div className={styles.card}>
       ProductCard
       <AddToCart />
     </div>
