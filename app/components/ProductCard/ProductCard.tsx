75c6c12574f279ec73fa4b51442f8b9245ba2097
Salino3 <flaviobcn012@gmail.com>
2026-05-02 12:58:29 +0200

    create dedicated folder

--- app/components/ProductCard/ProductCard.tsx
+++ app/components/ProductCard/ProductCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import AddToCart from "./AddToCart";
+import AddToCart from "../AddToCart";
 import styles from "./ProductCard.module.css";
 
 const ProductCard: React.FC = () => {
